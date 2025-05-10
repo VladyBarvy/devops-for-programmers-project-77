3083701ea94ed3511bcd940adcf61d787b49bbe7
VladyBarvy <vladbarskoff@yandex.ru>
2025-05-10 20:22:49 +0300

    one

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/VladyBarvy/devops-for-programmers-project-77/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/VladyBarvy/devops-for-programmers-project-77/actions)
+
+
+Доступные команды:
+- Шифрование файлов с секретами:
+```make encrypt_vault```
+
+- Расшифровывает файл Ansible vault для доступа к конфиденциальной информации:
+```make decrypt_vault```
+
+- Открывает файл Ansible vault для редактирования конфиденциальных данных:
+```make edit_vault```
+
+- Отображает содержимое файла Ansible Vault без редактирования:
+```make view_vault```
+
+- Извлечение образа Docker:
+```make pull-image```
+
+- Запуск контейнера Docker:
+```make run-container```
+
+- Настраивает мониторинг базы данных:
+```make configure_datadog```
+
+- Запуск формирования инфрастуктуры посредством Terraform:
+```make apply```
+
+Ссылка на приложение:
+```
+https://puffysound.ru
+```
---
+++ terraform/variables.tf
@@ -0,0 +1,73 @@
+variable "yc_iam_token" {
+  description = "Yandex Cloud Private Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_cloud_id" {
+  description = "Yandex Cloud cloud_id"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_folder_id" {
+  description = "Yandex Cloud folder_id"
+  type        = string
+  sensitive   = true
+}
+
+
+variable "pvt_key" {
+  description = "Path private key"
+  type        = string
+  sensitive   = true
+
+}
+variable "db_password" {
+  description = "Password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_username" {
+  description = "Username for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "organization_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "project_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "public_key" {
+  type        = string
+  description = "Public SSH key"
+}
+
+variable "user_data" {
+  type = map(any)
+  default = {
+    "cloud-init" = <<-EOF
+    #cloud-config
+    apt-update: true
+    apt-upgrade: true
+    EOF
+    "foo"        = "bar"
+  }
+}
