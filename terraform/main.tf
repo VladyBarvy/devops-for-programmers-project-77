80d48c920b025e9a4d9762034dbb80f139332652
VladyBarvy <vladbarskoff@yandex.ru>
2025-05-12 20:22:58 +0300

    fix 8_6

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,7 @@ resource "yandex_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("${path.module}/ssh/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${file("terraform/ssh/id_rsa.pub")}"
   }
 
 
@@ -120,7 +120,7 @@ EOT
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("${path.module}/ssh/id_rsa")
+    private_key = file("terraform/ssh/id_rsa")
     host        = self.network_interface.0.nat_ip_address
   }
 
